3f8620ac6112d92f2bc1f941720328fda6942b45
Aleksandr <aleksandr.sayko@gmail.com>
2017-02-03 22:00:01 +0200

    init project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { CharacterSheetComponent } from './character-sheet/character-sheet.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CharacterSheetComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
